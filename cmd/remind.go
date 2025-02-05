03fb3f14dd427d9b8d3ec37b0317563c8138ca81
Yossapol Witayanont <reef_3llen@hotmail.com>
2025-02-05 15:04:52 +0700

    feat: :rocket: remind

---
+++ cmd/remind.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// remindCmd represents the remind command
+var remindCmd = &cobra.Command{
+	Use:   "remind",
+	Short: "Notify me",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Parse the delay time with a unit (e.g., "10s", "5m", "1h")
+		delayTime, err := time.ParseDuration(args[0])
+		if err != nil {
+			fmt.Println("Invalid delay time. Please provide a valid duration (e.g., '10s', '5m', '1h').")
+			os.Exit(1)
+		}
+
+		// Get the notification message from the second argument
+		message := args[1]
+
+		// Start countdown
+		totalSeconds := int(delayTime.Seconds())
+		fmt.Printf("Countdown started for %s...\n", delayTime)
+
+		for i := totalSeconds; i > 0; i-- {
+			fmt.Printf("\rTime remaining: %d seconds", i)
+			time.Sleep(1 * time.Second)
+		}
+		fmt.Println("\nCountdown complete.")
+
+		// Send the macOS notification
+		scriptCmd := exec.Command("osascript", "-e", fmt.Sprintf(`display notification "%s" with title "Notification"`, message))
+		err = scriptCmd.Run()
+		if err != nil {
+			fmt.Println("Error sending notification:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Notification sent successfully!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(remindCmd)
+}
